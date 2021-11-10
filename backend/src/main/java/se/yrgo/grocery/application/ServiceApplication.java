324443e9ca702fc46ffd7b1d3e964d55892c0e48
Oscar Jonasson <Oscar.jonasson01@gmail.com>
2021-11-10 22:15:03 +0100

    adding application & serviceapplication for resteasy

---
+++ backend/src/main/java/se/yrgo/grocery/application/ServiceApplication.java
@@ -0,0 +1,36 @@
+package se.yrgo.grocery.application;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Application;
+
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
+import se.yrgo.grocery.rest.GroceryResourceService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationScoped
+public class ServiceApplication extends Application{
+	/**
+	 * Adding rest service class and cors to application.
+	 * 
+	 * Cors - Access to api
+	 */
+	private Set<Object> singletons = new HashSet<Object>();
+
+    public ServiceApplication() {
+    	CorsFilter corsfilter = new CorsFilter();
+        corsfilter.getAllowedOrigins().add("*");
+        corsfilter.setAllowedMethods("OPTIONS, GET, POST, PUT, DELETE, PATCH");
+        singletons.add(corsfilter);
+        singletons.add(new GroceryResourceService());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    
+}
