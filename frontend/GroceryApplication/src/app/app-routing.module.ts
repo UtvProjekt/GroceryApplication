3b0ca4209f8fa19ca2c45f984fee48a7fffe9112
AntonEwards <73063977+AntonEwards@users.noreply.github.com>
2021-11-14 22:07:43 +0100

    Customized header and added light dark switch

--- frontend/GroceryApplication/src/app/app-routing.module.ts
+++ frontend/GroceryApplication/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationsectionComponent } from './applicationsection/applicationsection.component';
+import { SigninpageComponent } from './signinpage/signinpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ApplicationsectionComponent },
+  { path: 'login', component: SigninpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/GroceryApplication/src/app/applicationsection/applicationsection.component.ts
+++ frontend/GroceryApplication/src/app/applicationsection/applicationsection.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'applicationsection',
+  selector: 'app-applicationsection',
   templateUrl: './applicationsection.component.html',
   styleUrls: ['./applicationsection.component.scss']
 })
 export class ApplicationsectionComponent implements OnInit {
+  darkOrLight: boolean = true
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  lightMode(): void{
+    document.documentElement.style.setProperty('--darkmode', '#fff')
+    document.documentElement.style.setProperty('--primary-text-color', '#0e0e0e')
+    
+  }
+  
+  darkMode(): void{
+    document.documentElement.style.setProperty('--darkmode', '#0e0e0e')
+    document.documentElement.style.setProperty('--primary-text-color', '#fff')
+  }
+
+  darkLightMode(): void{
+    if(this.darkOrLight){
+      this.lightMode()
+    }
+    else{
+      this.darkMode()
+    }
+    this.darkOrLight = !this.darkOrLight
   }
 
 }
