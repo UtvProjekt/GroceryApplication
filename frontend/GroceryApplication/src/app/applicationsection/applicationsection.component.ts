49e50e7fdff5331f8462c17058f2e6b87eb7256d
AntonEwards <73063977+AntonEwards@users.noreply.github.com>
2021-11-16 10:59:49 +0100

    Changed the lightswitch

--- frontend/GroceryApplication/src/app/applicationsection/applicationsection.component.ts
+++ frontend/GroceryApplication/src/app/applicationsection/applicationsection.component.ts
@@ -7,21 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationsectionComponent implements OnInit {
   darkOrLight: boolean = true
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
   lightMode(): void{
     document.documentElement.style.setProperty('--darkmode', '#fff')
     document.documentElement.style.setProperty('--primary-text-color', '#0e0e0e')
+    document.documentElement.style.setProperty('--primary-color-brighter', '#570057')
+    document.getElementById('header')!.style.boxShadow = '0 0 .3em .2em #d1d1d1'
+    document.getElementById('header')!.style.borderBottom = 'none'
+    document.getElementById('blockinbutton')!.style.transform = "translateX(2.3vw)"
     
   }
   
   darkMode(): void{
     document.documentElement.style.setProperty('--darkmode', '#0e0e0e')
     document.documentElement.style.setProperty('--primary-text-color', '#fff')
+    document.documentElement.style.setProperty('--primary-color-brighter', '#860086')
+    document.getElementById('header')!.style.boxShadow = '0 0 .3em .2em #860086'
+    document.getElementById('blockinbutton')!.style.transform = "translateX(0)"
+    
   }
 
   darkLightMode(): void{
