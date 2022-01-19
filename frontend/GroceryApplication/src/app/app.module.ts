7983fef7007a838b41711e35a006bb9a8026e28e
AntonEwards <73063977+AntonEwards@users.noreply.github.com>
2022-01-19 19:02:04 +0100

    Card slider

--- frontend/GroceryApplication/src/app/app.module.ts
+++ frontend/GroceryApplication/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';         
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ApplicationsectionComponent } from './applicationsection/applicationsection.component';
@@ -9,21 +11,12 @@ import { SearchbarComponent } from './searchbar/searchbar.component';
 import { ResultsComponent } from './results/results.component';
 import { SigninpageComponent } from './signinpage/signinpage.component';
 import { FooterComponent } from './footer/footer.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MyaccountComponent } from './myaccount/myaccount.component';
 import { SearchsectionComponent } from './searchsection/searchsection.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SettingsComponent } from './settings/settings.component';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', component: ApplicationsectionComponent },
-  { path: 'search', component: SearchsectionComponent },
-  { path: 'signin', component: SigninpageComponent },
-  { path: 'myaccount', component: MyaccountComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
-];
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -37,16 +30,14 @@ const routes: Routes = [
     MyaccountComponent,
     SearchsectionComponent,
     SettingsComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     FormsModule, ReactiveFormsModule,
-    HttpClientModule, 
-    [RouterModule.forRoot(routes, {
-      onSameUrlNavigation: 'reload',
-    })],
+    HttpClientModule,
 
   ],
   providers: [HeaderComponent],
