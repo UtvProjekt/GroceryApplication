a131cdf724d7ff8c2823a7ed588079f5bff3b033
AntonEwards <73063977+AntonEwards@users.noreply.github.com>
2022-01-20 22:23:51 +0100

    Small changes

--- solr-8.9.0/bin/stopsolr.bat
+++ solr-8.9.0/bin/stopsolr.bat
@@ -1,10 +1,1 @@
-@ECHO OFF
-ECHO =============
-ECHO SHUTING SOLR DOWN!
-ECHO =============
-
-ECHO ==================================
-ECHO STOPING SOLR ON PORT: 8983
-ECHO ==================================
-
 solr stop -all
